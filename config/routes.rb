90c67c96fc0ae0a8ba07c6716e35d3bef6b01da0
coralie delpha <coralie.delpha@gmail.com>
2020-05-17 23:31:52 +0200

    garden index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "gardens#index"
+
+  resources :gardens do
+    resources :plants, only: [:create]
+  end
+  resources :plants, only: :destroy
+
+  resources :plants do
+    resources :plant_tags, only: [ :new, :create ]
+  end
 end
+
