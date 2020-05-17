84d658082b3378d90aedeb8a2c8e843c93d1a59e
coralie delpha <coralie.delpha@gmail.com>
2020-05-17 23:27:48 +0200

    generate garden model and gardens controller

---
+++ app/controllers/gardens_controller.rb
@@ -0,0 +1,41 @@
+class GardensController < ApplicationController
+  before_action :set_garden, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @gardens = Garden.all
+  end
+
+  def show
+    @plant = Plant.new
+  end
+
+  def new
+    @garden = Garden.new
+  end
+
+  def edit
+  end
+
+  def create
+    @garden = Garden.new(garden_params)
+    @garden.save
+  end
+
+  def update
+   @garden.update(garden_params)
+  end
+
+  def destroy
+    @garden.destroy
+    redirect_to gardens_url
+  end
+
+  private
+    def set_garden
+      @garden = Garden.find(params[:id])
+    end
+
+    def garden_params
+      params.require(:garden).permit(:name, :banner_url)
+    end
+end
