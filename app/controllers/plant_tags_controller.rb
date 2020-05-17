cbbdbd7d5e982ec72243edf8d056fde12a7169d6
coralie delpha <coralie.delpha@gmail.com>
2020-05-17 23:59:16 +0200

    add a tag on a plant

--- app/controllers/plant_tags_controller.rb
+++ app/controllers/plant_tags_controller.rb
@@ -1,2 +1,36 @@
 class PlantTagsController < ApplicationController
+  def new
+    @plant = Plant.find(params[:plant_id])
+    @plant_tag = PlantTag.new
+  end
+
+  def create
+    #Step 1 : only one tag
+    # @plant = Plant.find(params[:plant_id])
+    # @tag = Tag.find(params[:plant_tag][:tag])
+    # @plant_tag = PlantTag.new
+    # @plant_tag.plant = @plant
+    # @plant_tag.tag = @tag
+    # plant_tag.save
+    # redirect_to garden_path(@plant.garden)
+
+    # Step 2 : multiple tags
+    @plant = Plant.find(params[:plant_id])
+    @tags = Tag.where(id: params[:plant_tag][:tag])
+    @tags.each do |tag|
+      plant_tag = PlantTag.new(plant: @plant, tag: tag)
+      if @plant_tag.save
+        redirect_to garden_path(@plant.garden)
+      else
+        render :new
+      end
+    end
+    redirect_to garden_path(@plant.garden)
+  end
+
+  def destroy
+    @plant_tag = PlantTag.find(params[:id])
+    @plant_tag.destroy
+    redirect_to garden_path(@plant_tag.plant.garden)
+  end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,7 @@
 class Tag < ApplicationRecord
   has_many :plant_tags, dependent: :destroy
   has_many :plants, through: :plant_tags
+
+  validates :tag, uniqueness: { scope: :plant,
+      message: "already added" }
 end
